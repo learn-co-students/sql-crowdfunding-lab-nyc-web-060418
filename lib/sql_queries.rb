2dfa7fc37965d3ad71c7cdba692706114e7c01c2
github username <github email address>
2018-06-13 17:42:47 -0400

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,24 +8,33 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "Write your SQL query Here"
+  "select projects.title, sum(amount) from pledges inner join projects on pledges.project_id = projects.id group by projects.title"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "Write your SQL query Here"
+"select users.name, users.age, sum(pledges.amount) from pledges inner join users where pledges.user_id = users.id group by users.name"
+
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
 "Write your SQL query Here"
+"select projects.title, sum(pledges.amount) - projects.funding_goal from projects inner join pledges where projects.id = pledges.project_id group by projects.id having sum(pledges.amount) >= projects.funding_goal"
+
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
 "Write your SQL query Here"
+"select users.name, sum(pledges.amount) from users inner join pledges where users.id = pledges.user_id group by users.name order by sum(pledges.amount)"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
 "Write your SQL query Here"
+"select projects.category, pledges.amount from projects inner join pledges where projects.id = pledges.project_id and projects.category = 'music'"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
 "Write your SQL query Here"
+"select projects.category, sum(pledges.amount) from projects inner join pledges where projects.id = pledges.project_id and projects.category = 'books'"
+
 end
